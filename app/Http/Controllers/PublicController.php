cae129344e6651745225c1b4c1d88ba6b18ed178
Sqtatdev <slava.ryvlin@gmail.com>
2026-01-29 09:35:19 +0200

    Add user page

--- app/Http/Controllers/PublicController.php
+++ app/Http/Controllers/PublicController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Like;
 use App\Models\Post;
+use App\Models\User;
 use Illuminate\Http\Request;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
